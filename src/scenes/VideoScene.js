2e6169d1b24cc20e0b0e48602ea4e14d215f5f38
walidvb <walidvb@gmail.com>
2018-09-10 19:03:22 +0200

    adds text on top of image

--- src/scenes/VideoScene.js
+++ src/scenes/VideoScene.js
@@ -1,20 +1,35 @@
+import $ from 'jquery'
+
 export default class VideoScene {
   constructor(description, container) {
     this.description = description;
     this.container = container;
   }
-  play(container) {
-    const video = this.createVideoDOM()
-    container.innerHTML = '';
-    container.appendChild(video);
-    video.addEventListener('ended', this.onEnd);
-    video.play();
+  play() {
+    this.$wrapper = $('<div class="video-container"></div>');
+    this.handleVideoDOM();
+    this.handleAttrs();
+    $(this.container).html(this.$wrapper);
   }
-  createVideoDOM(description = this.description){
-    const videoPlayerElement = document.createElement('video');
-    const source = document.createElement('source');
-    source.setAttribute('src', description.src);
-    videoPlayerElement.innerHTML = source.outerHTML;
-    return videoPlayerElement;
+  handleAttrs(){
+    const attrs = this.description.attrs;
+    if(attrs && attrs.length){
+      attrs.forEach( attr => {
+        const $elem = $(attr.dom);
+        setTimeout(() => {
+          $elem.appendTo(this.$wrapper);
+          if(attr.out){
+            setTimeout($elem.remove, attr.out);
+          }
+        }, attr.in || 0)
+      });
+    }
+  }
+  handleVideoDOM({ src } = this.description){
+    const $video = $(`<video controls autoplay="true"><source src=${src}/></video>`);
+    this.$wrapper.append($video);
+    const video = $video.get(0);
+    video.addEventListener('ended', this.onEnd);
+    return $video;
   }
 }

f231878aa2934e7ed3ebf7cfe4a3215c07efcbf9
walidvb <walidvb@gmail.com>
2018-09-11 18:04:37 +0200

    adds audio support

--- src/scenes/VideoScene.js
+++ src/scenes/VideoScene.js
@@ -8,9 +8,16 @@ export default class VideoScene {
   }
   play() {
     this.$wrapper = $('<div class="video-container"></div>');
-    this.handleVideoDOM();
-    this.handleAttrs();
+    this.video = this.handleVideo();
+    this.audio = this.handleAudio();
+    const endTrigger = this.video.duration > this.audio.duration ? this.video : this.audio;
+    endTrigger.addEventListener('ended', this.onEnd);
     $(this.container).html(this.$wrapper);
+    this.video.addEventListener('play', () =>{
+      this.handleAttrs();
+      this.audio.play();
+    });
+    this.video.play();
   }
   handleAttrs(){
     const attrs = this.description.attrs;
@@ -28,13 +35,16 @@ export default class VideoScene {
       });
     }
   }
-  handleVideoDOM({ src } = this.description){
-    const $video = $(`<video controls autoplay><source src=${src()}/></video>`);
+  handleAudio({ audioSrc } = this.description){
+    const $audio = $(`<audio src=${audioSrc}/></audio>`);
+    this.$wrapper.append($audio);
+    const audio = $audio.get(0);
+    return audio
+  }
+  handleVideo({ src } = this.description){
+    const $video = $(`<video><source src=${src}/></video>`);
     this.$wrapper.append($video);
     const video = $video.get(0);
-    video.addEventListener('ended', this.onEnd);
-    video.addEventListener('started', this.handleAttrs);
-    video.play();
-    return $video;
+    return video;
   }
 }

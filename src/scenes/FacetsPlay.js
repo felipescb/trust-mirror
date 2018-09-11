ce7a9209fbaf504ef086412f6402397757752320
walidvb <walidvb@gmail.com>
2018-09-12 00:42:59 +0200

    some styling

--- src/scenes/FacetsPlay.js
+++ src/scenes/FacetsPlay.js
@@ -3,12 +3,15 @@ import CountUp from 'countup';
 
 window.$ = $;
 // CONFIG
-const maxTopForStamps = 80;
-const minTopForStamps = 10;
-const maxFontSizeOffset = 1.5;
+const rowsCount = 6;
+
 
 export default function(data, onEnd){
   const $wrapper = $('<div class="facets-wrapper f-h a-c"></div>')
+  $wrapper.css({
+    //gridTemplateRows: `repeat(${rowsCount}, 1fr)`,
+    // transform: 'scaleX(-1)'
+  })
   const facets = flatten(
     data.raw.personality.map(big5 => 
       big5.children.map(c => {
@@ -21,15 +24,20 @@ export default function(data, onEnd){
   shuffleArray(facets);
   facets.forEach(createFacet);
 
+  let shown = 0;
   function showNext(){
+    let chosen;
     const hidden = $('.hidden');
-    const chosen = $(hidden[parseInt(Math.random()*hidden.length -1)]);
-    chosen.removeClass('hidden');
+    const rdm = parseInt(Math.random() * hidden.length - 1);
+    chosen = $(hidden[rdm]);
+    chosen.toggleClass('hidden clip-on');
     const numb = $('.count', chosen);
     const id = numb.attr('id')
     const count = parseInt(numb.text());
-    var countup = new CountUp(id, 0, count, 8, 3, { separator: '' })
-    console.log(numb)
+    var countup = new CountUp(id, 0, count, 0, 3, { 
+      separator: '', 
+      useEasing: false,
+    })
     countup.start();
     if(hidden.length > 0){
       setTimeout(showNext, Math.random()*300+600)
